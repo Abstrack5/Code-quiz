c796d124f25453fd7be50b38053014aba91767ca
Armon Ahmadi <abstrack@Armons-MacBook-Air.local>
2022-04-09 05:39:29 -0400

    quiz runs properly, 2 bugs aware of - timer countsdown faster after first time, and clear highscore doesnt let you see the list without refreshing.

--- script.js
+++ script.js
@@ -39,13 +39,24 @@ var welcomeText = document.getElementById("welcomeText");
 
 var viewHighScore = document.getElementById("viewHighScore");
 var playerScore = document.getElementById("userScore");
-// var userScorePoints = document.getElementById("userScorePoints")
+var finalScore = document.getElementById("finalScore");
+
+var initialInput = document.getElementById("initialInput");
+var submitInitialBtn = document.getElementById("submitInitialBtn");
+var summary = document.getElementById("summary");
+var highScoreSection = document.getElementById("highScoreSection");
+
+var goBackBtn = document.getElementById("goBackBtn");
+var clearHighScoreBtn = document.getElementById("clearHighScoreBtn"); 
+var viewHighScore = document.getElementById("viewHighScore");
+var listOfHighScores = document.getElementById("listOfHighScores");
 
 var timeLeft = document.getElementById("timeLeft");
 var timer = document.getElementById("timer");
+var timesUp = document.getElementById("timesUp");
 
 var startButton = document.getElementById("start-btn");
-var nextButton = document.getElementById("next-btn");
+var restartButton = document.getElementById("restart-btn");
 
 var container = document.getElementById("container");
 var questionContainerEl = document.getElementById("question-container");
@@ -56,32 +67,63 @@ var answerOne = document.getElementById("answerButton1");
 var answerTwo = document.getElementById("answerButton2");
 var answerThree = document.getElementById("answerButton3");
 var answerFour = document.getElementById("answerButton4");
+var answerCheck = document.getElementById("answerCheck");
 
 var shuffledQuestions = questions.sort(() => Math.random() - .5);
 var questionCountIndex = 0;
 
 var userScorePoints = 0;
-
+var totalTime = 81;
+var scoreResult;
 
 startButton.addEventListener("click", startGame);
-nextButton.addEventListener("click", showQuestion);
-// console.log(answerEl);
+restartButton.addEventListener("click", startGame);
+
+submitInitialBtn.addEventListener("click", function(event){ 
+    storeHighScores(event);
+});
+viewHighScore.addEventListener("click", function(event) { 
+    showHighScores(event);
+});
+goBackBtn.addEventListener("click", function() {
+    startButton.classList.remove("hide");
+    welcomeHeader.classList.remove("hide");
+    welcomeText.classList.remove("hide");
+    container.classList.remove("hide");
+    container.classList.remove("container");
+    questionContainerEl.classList.add("hide");
+    highScoreSection.classList.add("hide");
+});
+clearHighScoreBtn.addEventListener("click", function(){
+    window.localStorage.removeItem("high scores");
+    listOfHighScores.innerHTML = "High Scores Cleared!";
+    clearHighScoreBtn.classList.add("hide");
+    listOfHighScores.setAttribute("style", "font-family: 'Archivo', sans-serif; font-style: italic;")
+});
+
+
 for (var i = 0; i < answerEl.length; i++ ) {
     answerEl[i].addEventListener("click", checkAnswer);
 }
 
-function startTimer() {
-    var totalTime = 120;
 
-    var startTimer = setInterval(function() {
-        totalTime--;
-        timeLeft.textContent = totalTime;
-        if(totalTime <= 0) {
-            clearInterval(startTimer);
-            
-        }
-    },1000);
-}
+
+
+// function startTimer() {
+//     var startTimer = setInterval(function() {
+//         totalTime--;
+//         timeLeft.textContent = totalTime;
+//         if(totalTime <= 0) {
+//             clearInterval(startTimer);
+//             gameOver();
+//             if (questionCountIndex < questions.length - 1) {
+//                 timesUp.classList.add("hide");
+//                 gameOver();
+//             }
+//         };
+
+//     },1000);
+// }
 
 function startGame() {
     startButton.classList.add("hide");
@@ -91,8 +133,27 @@ function startGame() {
     timer.classList.remove("hide");
     playerScore.classList.remove("hide");
     questionContainerEl.classList.remove("hide");
-    // nextButton.classList.remove("hide");
-    startTimer();
+
+    initialInput.textContent = "";
+    questionCountIndex = 0;
+    totalTime = 80;
+    timeLeft.textContent = totalTime;
+
+
+    var startTimer = setInterval(function() {
+        totalTime--;
+        timeLeft.textContent = totalTime;
+        if(totalTime <= 0) {
+            clearInterval(startTimer);
+            gameOver();
+            if (questionCountIndex < questions.length - 1) {
+                timesUp.classList.add("hide");
+                gameOver();
+            }
+        };
+
+    },1000);
+    
 
     showQuestion();
 };
@@ -104,8 +165,6 @@ function showQuestion() {
     answerTwo.textContent = shuffledQuestions[questionCountIndex].b;
     answerThree.textContent = shuffledQuestions[questionCountIndex].c;
     answerFour.textContent = shuffledQuestions[questionCountIndex].d;
-
-    nextButton.classList.add("hide");
 };
 
 
@@ -113,48 +172,114 @@ function checkAnswer(event) {
     if (event.target.textContent === shuffledQuestions[questionCountIndex].correctAnswer) {
         userScorePoints += 5;
         playerScore.textContent = ("Score: " + userScorePoints);
-        nextButton.classList.remove("hide");
-        console.log("correct!");
-        // add another question
-         if (questionCountIndex <= shuffledQuestions.length) {
-            questionCountIndex++;
-            console.log("nextQuestion!");
-        }
-            // ends quiz after last question
-            if (questionCountIndex > shuffledQuestions.length - 1) {
-            console.log("GameOver");
-            // gameOver()
-            };  
-
-     }else {
-         timeLeft -= 5;
-        //  nextButton.classList.remove("hide");
-        console.log("Incorrect!");
+        answerCheck.classList.remove("hide");
+        answerCheck.textContent = "Correct!";
+     }
+     else {
+        totalTime -= 10;
+        answerCheck.classList.remove("hide");
+        answerCheck.textContent = "Wrong! The correct answer was: " + " " + questions[questionCountIndex].correctAnswer;
+        answerCheck.classList.add("text-danger");
         };
+
+        questionCountIndex++;
+
+    // add another question
+    if (questionCountIndex < shuffledQuestions.length) {
+        // questionCountIndex++;
+        showQuestion();
+    } else {
+        gameOver();
+    }
+
+}
+
+
+function gameOver() {
+    container.classList.add("hide");
+    answerCheck.classList.add("hide");
+    playerScore.classList.add("hide");
+    timer.classList.add("hide");
+    welcomeHeader.classList.add("hide");
+    welcomeText.classList.add("hide");
+    summary.classList.remove("hide");
+
+    finalScore.textContent = userScorePoints
+}
+
+
+// enter initial and store highscore in local storage
+function storeHighScores(event) {
+    event.preventDefault();
+
     
-};
+    // stop function is initial is blank
+    if (initialInput.value === "") {
+        alert("Please enter your initials!");
+        return;
+    } 
+    
+    container.classList.add("hide");
+    answerCheck.classList.add("hide");
+    playerScore.classList.add("hide");
+    timer.classList.add("hide");
+    summary.classList.remove("hide");
+    highScoreSection.classList.remove("hide");
 
 
 
+    // store scores into local storage
+    var savedHighScores = localStorage.getItem("high scores");
+    var scoresArray;
 
+    if (savedHighScores === null) {
+        scoresArray = [];
+    } else {
+        scoresArray = JSON.parse(savedHighScores)
+    }
 
+    var userScore = {
+        initials: initialInput.value,
+        score: finalScore.textContent
+    };
 
+    console.log(userScore);
+    scoresArray.push(userScore);
 
+    // stringify array in order to store in local
+    var scoresArrayString = JSON.stringify(scoresArray);
+    window.localStorage.setItem("high scores", scoresArrayString);
+    
+    // show current highscores
+    showHighScores();
+}
 
-// add correct or incorrect message? 
-// choice box change to red for incorrect, green for correct?
+// function to show high scores
+var i = 0;
+function showHighScores() {
 
-// add gameover function to shower restart or view highscores
-// Timer, setinterval, stop timer after last question, add timer clock
-// highscore/localstorage
+    container.classList.add("hide");
+    answerCheck.classList.add("hide");
+    playerScore.classList.add("hide");
+    timer.classList.add("hide");
+    summary.classList.add("hide");
+    welcomeHeader.classList.add("hide");
+    welcomeText.classList.add("hide");
+    highScoreSection.classList.remove("hide");
 
+    var savedHighScores = localStorage.getItem("high scores");
 
-// if (correct) {
-//     Element.classlist.add('correct')
-// } else {
-//     Element.classlist.add('wrong')
+    // check if there is any in local storage
+    if (savedHighScores === null) {
+        return;
+    }
+    console.log(savedHighScores);
 
-// }
+    var storedHighScores = JSON.parse(savedHighScores);
 
-// Element.classlist.remove('wrong')
-// Element.classlist.remove('wrong')
+    for (; i < storedHighScores.length; i++) {
+        var eachNewHighScore = document.createElement("p");
+        eachNewHighScore.innerHTML = storedHighScores[i].initials + ": " + storedHighScores[i].score;
+        listOfHighScores.appendChild(eachNewHighScore);
+    }
+}
